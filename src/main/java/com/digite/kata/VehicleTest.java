cbfc1ec8f49535b7c7e8ae80c2b806b7636b035b
poonamalai <85272623+poonamalai@users.noreply.github.com>
2021-06-03 11:24:19 +0530

    Update VehicleTest.java

--- src/main/java/com/digite/kata/VehicleTest.java
+++ src/main/java/com/digite/kata/VehicleTest.java
@@ -1,1 +1,63 @@
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.digite.kata.Vehicle;
+
+public class JTestVehicle {
+	
+	@Test
+	public void testgetMaxFuel() {
+		
+		Vehicle w_vehicle = new Vehicle(100);
+		
+		assertEquals(100, w_vehicle.getMaxFuel());
+	}
+  
+  @Test
+	public void testgetRemainingFuel() {
+		
+		Vehicle w_vehicle = new Vehicle(100);
+		
+		assertEquals(100, w_vehicle.getRemainingFuel());	
+	}
+  
+  @Test
+	public void testSetRemainingFuel() {
+		
+		Vehicle w_vehicle = new Vehicle(100);
+    VehicleFuel w_vehicleFuel = new VehicleFuel();
+		
+    w_vehicleFuel.setRemainingFuel(w_Vehicle, 40);
+    
+		assertEquals(40, w_vehicle.getRemainingFuel());	
+	}
+  
+  @Test
+	public void testSetReFuel() {
+		
+		Vehicle w_vehicle = new Vehicle(100);
+    VehicleFuel w_vehicleFuel = new VehicleFuel();
+		
+    w_vehicleFuel.setRemainingFuel(w_Vehicle, 40);
+    
+    w_vehicleFuel.setReFuel(w_Vehicle);
+    
+		assertEquals(100, w_vehicle.getRemainingFuel());	
+	}
+  
+  @Test
+	public void testAccelerate() {
+		
+		Vehicle w_vehicle = new Vehicle(100);
+    VehicleAction w_vehicleAction = new VehicleAction(100);
+    
+    w_vehicleAction.accelerate(w_vehicle);
+    
+    assertEquals(99, w_vehicle.getRemainingFuel());	
+	}
+  
+  	
+
+}
